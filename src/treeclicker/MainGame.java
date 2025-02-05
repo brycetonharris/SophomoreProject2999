f3d26d5d240d98a409b587fa824b364ac39dc660
awilliam26 <stnwilliams667@gmail.com>
2025-02-04 19:04:15 -0500

    Added a treeclicker logo and modified GUI.

--- src/treeclicker/MainGame.java
+++ src/treeclicker/MainGame.java
@@ -7,18 +7,21 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 import javafx.scene.paint.Color;
+import java.net.URL;
 
 public class MainGame extends Application{
 	
 	 public void start(Stage primaryStage) {    	
 	    	
 	        try {
-	        //Load the FXML file
+	        // Load the FXML file
 	        FXMLLoader loader = new FXMLLoader(getClass().getResource("/resources/game.fxml"));
 	        
+	        // code from UIapp.java
 	        Button btn = new Button();
 	        btn.setText("'Chop Tree Button'");
 	        btn.setOnAction(new EventHandler<ActionEvent>() {
@@ -27,17 +30,33 @@ public class MainGame extends Application{
 	            public void handle(ActionEvent event) {
 	            	System.out.println("whatever function goes after this");
 	            }});
+	        // end of code from UIapp.java
 	        
 	        Parent root = loader.load();               
 
-	        Scene scene = new Scene(root, 300, 250);
+	        Scene scene = new Scene(root, 600, 600);
 	        root.setStyle("-fx-background-color: aquamarine;");
-	        primaryStage.setTitle("JavaFX Game");
+
+	        URL imageURL = getClass().getResource("/resources/treeclicker logo.png");
+	        
+            if (imageURL != null) {
+            	
+                Image icon = new Image(imageURL.toExternalForm());
+                primaryStage.getIcons().add(icon);  // Set the window icon
+                
+            } else {
+            	
+                System.out.println("Image not found!");
+            }
+            
+            //primaryStage.setFullScreen(true);
+
+	        primaryStage.setTitle("TreeClicker");
 	        primaryStage.setScene(scene);
 	        primaryStage.show();
 	        
 	        } catch (Exception e) {
-	        	e.printStackTrace(); //Handle any loading errors
+	        	e.printStackTrace(); // Handle any loading errors
 	        }
 	    }
 
