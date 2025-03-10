9d77d5ae32d796c05c3ca593fd9343a7a3daae9e
ShaneH11 <shanehyatt@oakland.edu>
2025-03-10 16:23:24 -0400

    Update TreeWeatherTime.java
    I fixed the error and now it is not in the main

--- src/treeweathertimer/TreeWeatherTime.java
+++ src/treeweathertimer/TreeWeatherTime.java
@@ -1,65 +1,73 @@
-package treeweathertimer; 
-
+package treeweathertimer;
 
 import java.util.Timer;
 import java.util.TimerTask;
 
-/*
-public class TreeWeatherTime {
+import treeclicker.Controller;
+import weather.Weather;
+
+
+public class TreeWeatherTimer {
+	public String WeatherState = "Clear";
+	public TreeWeatherTimer(Controller controller) {
+		//timer
+	    Timer timer = new Timer();
+	    TimerTask task = new TimerTask() {
+	    	int Sec = 0;
+	    	int Min = 0;
+			@Override
+			public void run() {
+				//timeLabel.setText(counter + "Seconds");
+				if(Sec == 60) {
+					Sec -= 60;
+					++Min;
+				}
+					System.out.println(Min + ":" + Sec);
+					Sec++;
+			}
+	    };
+	    
+	    int [] Weather = {1,2,3,4};
+	    Weather W = new Weather(WeatherState);
+	       
+	    TimerTask WeatherTime = new TimerTask() {
 	
-	 //timer 
-            Timer timer = new Timer();
-	        TimerTask task = new TimerTask() {
-	        	int Sec = 0;
-	        	int Min = 0;
-				@Override
-				public void run() {
-					//timeLabel.setText(counter + "Seconds");
-					if(Sec == 60) {
-						Sec -= 60;
-						++Min;
-					}
-						System.out.println(Min + ":" + Sec);
-						Sec++;
+			@Override
+			public void run() {
+				if(WeatherState == "Sunny"){
+					W.SunnyOff(); 
+				}else if(WeatherState == "Snow") {
+					W.SnowOff();
+				}else if(WeatherState == "Rain"){
+					W.rainOff();
 				}
-	        };
-	        
-	        int [] Weather = {1,2,3,4};
-	        Weather W = new Weather(WeatherState);
-	           
-	        TimerTask WeatherTime = new TimerTask() {
-
-				@Override
-				public void run() {
-					if(WeatherState == "Sunny"){
-						W.SunnyOff(); 
-					}else if(WeatherState == "Snow") {
-						W.SnowOff();
-					}else if(WeatherState == "Rain"){
-						W.rainOff();
-					}
-					
-					
-					int randomNumber = (int) (Math.random() * 4);
-					if(Weather[randomNumber] == 1) {
-						WeatherState = "Sunny";
-						W.SunnyOn();
-					}else if(Weather[randomNumber] == 2){
-						 WeatherState = "Clear";
-					}else if(Weather[randomNumber] == 3){
-						 WeatherState = "Rain";
-						 W.rainOn();
-						 
-					}else {
-						 WeatherState = "Snow";
-						 W.SnowOn(); 
-					}
+				
+				
+				int randomNumber = (int) (Math.random() * 4);
+				if(Weather[randomNumber] == 1) {
+					WeatherState = "Sunny";
+					W.SunnyOn();
+				}else if(Weather[randomNumber] == 2){
+					 WeatherState = "Clear";
+				}else if(Weather[randomNumber] == 3){
+					 WeatherState = "Rain";
+					 W.rainOn();
+					 controller.BackGroundChangeRain();
+					 
+				}else {
+					 WeatherState = "Snow";
+					 W.SnowOn(); 
+					 controller.BackGroundChangeSnow();
 				}
-	        	
-	        };
-	        //Timer for the Game
-	        timer.scheduleAtFixedRate(task, 0, 1000);
-	        //Timer event for the weather
-	        timer.scheduleAtFixedRate(WeatherTime, 0, 150000); 
+			}
+	    	
+	    };
+	    //Timer for the Game
+	    timer.scheduleAtFixedRate(task, 0, 1000);
+	    //Timer event for the weather
+	    timer.scheduleAtFixedRate(WeatherTime, 0, 150000);
+	}
 
-} */
+
+}
+		
