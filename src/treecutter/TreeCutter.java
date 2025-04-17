825683c78646fcd7e970e61a763fb9cb92cc243d
awilliam26 <stnwilliams667@gmail.com>
2025-04-16 20:45:55 -0400

    removed dead code.

--- src/treecutter/TreeCutter.java
+++ src/treecutter/TreeCutter.java
@@ -41,35 +41,5 @@ public class TreeCutter {
 	public void setCutter(String cutter) {
 		
 		this.cutter = cutter;
-	}
-	
-	/* players can upgrade their cutting tool.
-	public void upgradeCutter() {
-		
-		// upgrade axe to chainsaw.
-		if(points >= 10000 && cutter.equals("Axe")) {
-			this.cutter = "Chainsaw";
-			
-			this.damage = 1.25;
-			
-			points -= 10000;
-			
-			System.out.println("Upgraded to Chainsaw! Remaining points: " + points);
-			
-		} else if(points >= 50000 && cutter.equals("Chainsaw")) { // upgrade chainsaw to laser.
-			this.cutter = "Laser";
-			
-			this.damage = 1.50;
-						
-			points -= 50000;
-			
-			System.out.println("Upgraded to Laser! Remaining points: " + points);
-			
-		} else {
-			
-			System.out.println("Not enough points to upgrade or already upgraded to highest tool");
-		}
-		
-	} */
-	
+	}	
 }
